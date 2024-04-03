6936f4cb655187c5155600db9739877099cef123
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-02 21:42:32 -0300

    Update escolaRoutes.ts

--- src/routes/escolaRoutes.ts
+++ src/routes/escolaRoutes.ts
@@ -1,31 +1,31 @@
-import express, { Request, Response } from "express";
-import EscolaController from "../controllers/escolaController";
+import express, { Request, Response } from 'express';
+import EscolaController from '../controllers/escolaController';
 
 const router = express.Router();
 
-// Get all escolas
-router.get("/", async (req: Request, res: Response) => {
-  await EscolaController.getAllEscolas(req, res);
+// Get escola by CPF
+router.get("/cpf/:cpf", async (req: Request, res: Response) => {
+  await EscolaController.getEscolaByCPF(req, res);
 });
 
-// Get escola by Codigo INEP
-router.get("/codigoInep/:codigoInep", async (req: Request, res: Response) => {
-  await EscolaController.getEscolaByCodigoInep(req, res);
+// Get all escolas
+router.get('/', async (req: Request, res: Response) => {
+    await EscolaController.getAllEscolas(req, res);
 });
 
 // Create new escola
-router.post("/", async (req: Request, res: Response) => {
-  await EscolaController.createEscola(req, res);
+router.post('/', async (req: Request, res: Response) => {
+    await EscolaController.createEscola(req, res);
 });
 
-// Update escola by Codigo INEP
-router.put("/:codigoInep", async (req: Request, res: Response) => {
-  await EscolaController.updateEscola(req, res);
+// Update escola by CPF
+router.put("/cpf/:cpf", async (req: Request, res: Response) => {
+  await EscolaController.updateEscolaByCPF(req, res);
 });
 
-// Delete escola by Codigo INEP
-router.delete("/:codigoInep", async (req: Request, res: Response) => {
-  await EscolaController.deleteEscola(req, res);
+// Delete escola by CPF
+router.delete("/cpf/:cpf", async (req: Request, res: Response) => {
+  await EscolaController.deleteEscolaByCPF(req, res);
 });
 
 export default router;
