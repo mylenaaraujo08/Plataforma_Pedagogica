a37e3c6c73ca253c8c9badca9206d17b68e67767
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-02 21:06:55 -0300

    Update gestorModel.ts

--- src/models/gestorModel.ts
+++ src/models/gestorModel.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
 class GestorModel {
   static pool = new Pool({
@@ -8,31 +8,46 @@ class GestorModel {
     connectionString: process.env.DATABASE_URL,
   });
 
+  id?: string;
+  nome_completo?: string;
   cpf?: string;
-  nome?: string;
-  escola?: string;
+  email?: string;
   funcao?: string;
-  telefones?: string[];
-  login?: string;
+  telefone?: string;
+  celular?: string;
 
   constructor(data: any = {}) {
+    this.id = data.id || undefined;
+    this.nome_completo = data.nome_completo || undefined;
     this.cpf = data.cpf || undefined;
-    this.nome = data.nome || undefined;
-    this.escola = data.escola || undefined;
+    this.email = data.email || undefined;
     this.funcao = data.funcao || undefined;
-    this.telefones = data.telefones || [];
-    this.login = data.login || undefined;
+    this.telefone = data.telefone || undefined;
+    this.celular = data.celular || undefined;
   }
 
-  static async findByCPF(cpf: string): Promise<GestorModel | undefined> {
-    const result = await this.pool.query(
-      `
-        SELECT *
-        FROM gestores
-        WHERE cpf = $1
-      `,
-      [cpf]
-    );
+  static async findById(id: string | null, cpf: string | null, nome: string | null): Promise<GestorModel | undefined> {
+    let query = `
+      SELECT *
+      FROM gestores
+      WHERE 1 = 1
+    `;
+    const values = [];
+
+    if (id) {
+      query += ` AND id = $${values.length + 1}`;
+      values.push(id);
+    }
+    if (cpf) {
+      query += ` AND cpf = $${values.length + 1}`;
+      values.push(cpf);
+    }
+    if (nome) {
+      query += ` AND nome_completo = $${values.length + 1}`;
+      values.push(nome);
+    }
+
+    const result = await this.pool.query(query, values);
     return result.rows[0] ? new GestorModel(result.rows[0]) : undefined;
   }
 
@@ -50,22 +65,24 @@ class GestorModel {
     await this.pool.query(
       `
         INSERT INTO gestores (
+          id,
+          nome_completo,
           cpf,
-          nome,
-          escola,
+          email,
           funcao,
-          telefones,
-          login
+          telefone,
+          celular
         )
-        VALUES ($1, $2, $3, $4, $5, $6)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
       `,
       [
+        gestor.id,
+        gestor.nome_completo,
         gestor.cpf,
-        gestor.nome,
-        gestor.escola,
+        gestor.email,
         gestor.funcao,
-        gestor.telefones,
-        gestor.login,
+        gestor.telefone,
+        gestor.celular
       ]
     );
     return gestor;
@@ -76,27 +93,79 @@ class GestorModel {
       `
         UPDATE gestores
         SET
-          nome = $1,
-          escola = $2,
-          funcao = $3,
-          telefones = $4,
-          login = $5
-        WHERE cpf = $6
+          nome_completo = $1,
+          cpf = $2,
+          email = $3,
+          funcao = $4,
+          telefone = $5,
+          celular = $6
+        WHERE id = $7
       `,
       [
-        gestor.nome,
-        gestor.escola,
-        gestor.funcao,
-        gestor.telefones,
-        gestor.login,
+        gestor.nome_completo,
         gestor.cpf,
+        gestor.email,
+        gestor.funcao,
+        gestor.telefone,
+        gestor.celular,
+        gestor.id,
       ]
     );
     return gestor;
   }
 
-  static async deleteByCPF(cpf: string): Promise<void> {
-    await this.pool.query("DELETE FROM gestores WHERE cpf = $1", [cpf]);
+  static async excluirPorId(id: string): Promise<void> {
+    await this.pool.query(
+      `
+        DELETE FROM gestores
+        WHERE id = $1
+      `,
+      [id]
+    );
+  }
+
+  static async findByCpf(cpf: string): Promise<GestorModel | undefined> {
+    const result = await this.pool.query(
+      `
+        SELECT *
+        FROM gestores
+        WHERE cpf = $1
+      `,
+      [cpf]
+    );
+    return result.rows[0] ? new GestorModel(result.rows[0]) : undefined;
+  }
+
+  static async findByNome(nome: string): Promise<GestorModel | undefined> {
+    const result = await this.pool.query(
+      `
+        SELECT *
+        FROM gestores
+        WHERE nome_completo = $1
+      `,
+      [nome]
+    );
+    return result.rows[0] ? new GestorModel(result.rows[0]) : undefined;
+  }
+
+  static async deleteByCpf(cpf: string): Promise<void> {
+    await this.pool.query(
+      `
+        DELETE FROM gestores
+        WHERE cpf = $1
+      `,
+      [cpf]
+    );
+  }
+
+  static async deleteByNome(nome: string): Promise<void> {
+    await this.pool.query(
+      `
+        DELETE FROM gestores
+        WHERE nome_completo = $1
+      `,
+      [nome]
+    );
   }
 }
 
