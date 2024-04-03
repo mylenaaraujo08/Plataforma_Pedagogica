91f79dae7fa5b395c4ba445b22f0b27fbcba9578
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-02 21:09:01 -0300

    Update indexControllers.ts

--- src/controllers/indexControllers.ts
+++ src/controllers/indexControllers.ts
@@ -2,5 +2,6 @@ import AlunoController from "./alunoController";
 import EscolaController from "./escolaController";
 import GestorController from "./gestorController";
 import TurmaController from "./turmaController";
+import CadastroController from "./cadastroController"; 
 
-export { AlunoController, EscolaController, GestorController, TurmaController };
+export { AlunoController, EscolaController, GestorController, TurmaController, CadastroController }; 
