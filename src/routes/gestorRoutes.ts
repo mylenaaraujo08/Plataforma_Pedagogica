197c6d701f1ffb97075fedc9db98fa23220f0008
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-02 21:43:54 -0300

    Update gestorRoutes.ts

--- src/routes/gestorRoutes.ts
+++ src/routes/gestorRoutes.ts
@@ -1,31 +1,31 @@
 import express, { Request, Response } from 'express';
-import EscolaController from '../controllers/escolaController';
+import GestorController from '../controllers/gestorController';
 
 const router = express.Router();
 
-// Get escola by CPF
-router.get("/cpf/:cpf", async (req: Request, res: Response) => {
-  await EscolaController.getEscolaByCPF(req, res);
-});
-
-// Get all escolas
+// Get all gestores
 router.get('/', async (req: Request, res: Response) => {
-    await EscolaController.getAllEscolas(req, res);
+    await GestorController.getAllGestores(req, res);
 });
 
-// Create new escola
+// Get gestor by ID
+router.get('/:id', async (req: Request, res: Response) => {
+    await GestorController.getGestorById(req, res);
+});
+
+// Create new gestor
 router.post('/', async (req: Request, res: Response) => {
-    await EscolaController.createEscola(req, res);
+    await GestorController.createGestor(req, res);
 });
 
-// Update escola by CPF
-router.put("/cpf/:cpf", async (req: Request, res: Response) => {
-  await EscolaController.updateEscolaByCPF(req, res);
+// Update gestor by ID
+router.put('/:id', async (req: Request, res: Response) => {
+    await GestorController.updateGestor(req, res);
 });
 
-// Delete escola by CPF
-router.delete("/cpf/:cpf", async (req: Request, res: Response) => {
-  await EscolaController.deleteEscolaByCPF(req, res);
+// Delete gestor by ID
+router.delete('/:id', async (req: Request, res: Response) => {
+    await GestorController.deleteGestor(req, res);
 });
 
 export default router;
